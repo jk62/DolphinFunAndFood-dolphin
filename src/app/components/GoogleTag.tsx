893639e4f1a9e84f2717791f6528a83c15ddf513
jk62 <jaikishanbhargav@yahoo.com>
2025-12-13 18:32:14 +0530

    GoogleTag incorporated

--- src/app/components/GoogleTag.tsx
+++ src/app/components/GoogleTag.tsx
@@ -1,6 +1,5 @@
 // src/app/components/GoogleTag.tsx
 "use client";
-
 import Script from "next/script";
 
 const GOOGLE_ADS_ID = "AW-1776989853";
@@ -10,20 +9,53 @@ export default function GoogleTag() {
     <>
       {/* Load the Google tag script */}
       <Script
-        id="google-ads-gtag"
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ADS_ID}`}
       />
-
-      {/* Initialize gtag */}
-      <Script id="google-ads-gtag-inline" strategy="afterInteractive">
-        {`
+      {/* Initialize gtag after loading */}
+      <Script
+        id="google-gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){ dataLayer.push(arguments); }
           gtag('js', new Date());
           gtag('config', '${GOOGLE_ADS_ID}');
-        `}
-      </Script>
+        `,
+        }}
+      />
     </>
   );
 }
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! //
+// // src/app/components/GoogleTag.tsx
+// "use client";
+
+// import Script from "next/script";
+
+// const GOOGLE_ADS_ID = "AW-1776989853";
+
+// export default function GoogleTag() {
+//   return (
+//     <>
+//       {/* Load the Google tag script */}
+//       <Script
+//         id="google-ads-gtag"
+//         strategy="afterInteractive"
+//         src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ADS_ID}`}
+//       />
+
+//       {/* Initialize gtag */}
+//       <Script id="google-ads-gtag-inline" strategy="afterInteractive">
+//         {`
+//           window.dataLayer = window.dataLayer || [];
+//           function gtag(){dataLayer.push(arguments);}
+//           gtag('js', new Date());
+//           gtag('config', '${GOOGLE_ADS_ID}');
+//         `}
+//       </Script>
+//     </>
+//   );
+// }
